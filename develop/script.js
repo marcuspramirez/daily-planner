646e8ed0d869b386ec4d67b85606ce2854eb5653
marcuspramirez <marcus.p.ramirez2020@gmail.com>
2020-08-02 09:12:17 -0500

    completed script

--- develop/script.js
+++ develop/script.js
@@ -2,6 +2,7 @@
 $(document).ready(function () {
     var rightNow = moment().format('MMMM Do YYYY, h:mm:ss a');
     var hourNow = moment().hours();
+    const baseSavedValues = JSON.stringify({});
 
     // What is .each?  What is index?  How are we using this?  New block variable creating an ID attribute?
     // This should be changing the colors according to the current time
@@ -31,23 +32,30 @@ $(document).ready(function () {
 
         // Trying to save to local storage, not working.  Why the empty array?
 
-        var obj = JSON.parse(localStorage.getItem("saveTodo")) || [];
-        obj.push({
-            time: savedTime,
-            value: savedValue
-        })
+
+        var obj = JSON.parse(localStorage.getItem("saveTodo") || baseSavedValues);
+       
+        obj[savedTime] = savedValue
+       
 
         localStorage.setItem("saveTodo", JSON.stringify(obj));
-        $(".time-block").each(function (index) {
-            console.log(savedValue);
-            console.log(savedTime);
-        })
 
     })
 
     // get the items from the array
-    let saveItems = JSON.parse(localStorage.getItem("saveTodo"))
+    let saveItems = JSON.parse(localStorage.getItem("saveTodo") || baseSavedValues)
     console.log(saveItems);
+
+    const saveItemKeys = Object.keys(saveItems);
+
+    if(saveItemKeys.length > 0){
+        for (let i = 0; i < saveItemKeys.length; i++) {
+            const key = saveItemKeys[i];
+            $('#' + key).find('.description').text(saveItems[key]);
+        }
+    }
+    //{9: 0}
+    //[{time: 9, value: hellow world}]
 
     // for each description, from the id == which is the time it was saved, and store the item.
     // $(".description").each(function (index, value) {
